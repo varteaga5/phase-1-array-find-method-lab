b7c40cc303cc974a07309f78528a3f2d11c9b8c1
vincent <vincent.arteaga@gmail.com>
2021-03-24 23:41:54 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin (record) {
+    const x = record.find(isW)
+    if (x) {
+    return x.year
+    } else {
+        return undefined
+    }
+}
 
+function isW(element, index, array) {
+    return (element.result === 'W')
+}
